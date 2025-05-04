2b7fa5448705a37ca2f7f1395cb740f42fa3ede0
apartigor <contact.jojigor@gmail.com>
2025-05-03 22:52:50 -0300

    feat(back): :sparkles: Configuração do banco no controller da Home e .gitignore atualizado

--- bytelibAPI/Controllers/HomeController.cs
+++ bytelibAPI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using bytelibAPI.Data;
 
 namespace bytelibAPI.Controllers
 {
@@ -7,12 +8,12 @@ namespace bytelibAPI.Controllers
     [Route("")]  // Agora a rota para o HomeController será /home
     public class HomeController : ControllerBase
     {
-        // private readonly AppDbContext _appDbContext;
+        private readonly AppDbContext _appDbContext;
 
-        // public HomeController(AppDbContext appDbContext)
-        // {
-        //     _appDbContext = appDbContext;
-        // }
+        public HomeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
         // Responde a uma requisição GET para /home
         [HttpGet]
         public IActionResult Index()
